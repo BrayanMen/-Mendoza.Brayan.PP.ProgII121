71c9cc014e7e1264b395295322ae343b35e529e5
Brayan Mendoza <95663591+BrayanMen@users.noreply.github.com>
2024-11-11 23:39:42 -0300

    Delete src/mendoza/brayan/pp/progii121 directory

--- src/mendoza/brayan/pp/progii121/enums/Dieta.java
+++
@@ -1,7 +0,0 @@
-package mendoza.brayan.pp.progii121.enums;
-
-public enum Dieta {
-    HERBIVORO,
-    CARNIVORO,
-    OMNIVORO
-}
--- src/mendoza/brayan/pp/progii121/exception/ExceptionAnimalesVacuna.java
+++
@@ -1,8 +0,0 @@
-package mendoza.brayan.pp.progii121.exception;
-
-public class ExceptionAnimalesVacuna extends RuntimeException{
-
-    public ExceptionAnimalesVacuna(String message) {
-        super(message);
-    }
-}
